b4a9ce38170893b62bbf02c3c9ab9151e9b82a68
William Lin <willlin1996@gmail.com>
2020-03-12 16:23:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,9 +4,11 @@ attr_accessor :name
   def initialize(name)
     @name=name 
   end
+  
   def songs
     Songs.all.select do |song_object|
       song_object.artist == self
     end
   end
+  
 end
